b72c98c449b2eb123fbb38313b2f4832c838a8a5
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-07-21 12:35:13 +0500

    redirect url

--- default/connect.php
+++ default/connect.php
@@ -3,6 +3,8 @@
 require 'header.php';
 include 'config.php';
 
+$redirect_url = $_SERVER['REDIRECT_URL'];
+
 $mac = $_SESSION["id"];
 $apmac = $_SESSION["ap"];
 $user_type = $_SESSION["user_type"];
@@ -54,7 +56,7 @@ $auth_result = $unifi_connection->authorize_guest($mac, $duration, null, null, n
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
     <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
-    <meta http-equiv="refresh" content="5;url=https://www.google.com" />
+    <meta http-equiv="refresh" content="5;url=<?php echo htmlspecialchars($redirect_url); ?>" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
     <link rel="stylesheet" href="../assets/styles/style.css" />
